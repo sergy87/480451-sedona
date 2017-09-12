a62c70245b650c8955028705080b184f9493dbed
sergy87 <sir.serfer87@yandex.ru>
2017-09-12 10:24:10 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var link = document.querySelector(".search a");
 var	form = document.querySelector(".map form.form");
-form.classList.add("form_hiden"); //добавляем класс скрывающий форму
+form.classList.add("form_hiden");
 link.addEventListener("click", function (evt) { 
 	evt.preventDefault();
 if(form.classList.contains("form_hiden")&&!form.classList.contains("animation")&&!form.classList.contains("animation_close")){
