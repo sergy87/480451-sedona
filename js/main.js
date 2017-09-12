97707c260423c64cd37fb319a7f0b0684e56a319
sergy87 <sir.serfer87@yandex.ru>
2017-09-12 12:19:20 +0300

    Add files via upload
    добавлен работающий скрипт, сжаты файлы

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 var link = document.querySelector(".search a");
 var	form = document.querySelector(".map form.form");
-form.classList.add("form_hiden");
-link.addEventListener("click", function (evt) { 
-	evt.preventDefault();
-if(form.classList.contains("form_hiden")&&!form.classList.contains("animation")&&!form.classList.contains("animation_close")){
-	form.classList.toggle("form_hiden");
-	form.classList.add("animation");
-}else {
-	form.classList.toggle("animation");
-	form.classList.toggle("animation_close");
-	form.classList.toggle("form_hiden");
-}
-}); 
+
+
+window.addEventListener("load", function(event) {
+        if(!form.classList.contains("form_hiden")) {
+            form.classList.add("form_hiden");
+        }
+    }
+);
+
+
+link.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        if(form.classList.contains("form_hiden")) {
+
+            form.classList.remove("form_hiden");
+            form.classList.add("animation");
+        }
+         else {
+            form.classList.remove("animation");
+            form.classList.add("form_hiden");
+         }
+    }
+);
